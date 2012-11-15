efcc03f3d74c7258f312be239af440ff2f33397d
顏孜羲 <joseph.yen@gmail.com>
2012-11-15 20:04:10 +0800

    homebrew coreutils as default

--- bin/zsh_basic.zsh
+++ bin/zsh_basic.zsh
@@ -9,5 +9,6 @@ alias d=popd
 export LESS="-FXRS"
 export EDITOR="ec"
 export PATH="/Users/joe/bin:/home/joe/bin:/usr/local/bin:$PATH"
+PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 export LC_ALL=en_US.UTF-8
