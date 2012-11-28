fb4bff96cf0bf9ffb1accc91c189569c080fe297
顏孜羲 <joseph.yen@gmail.com>
2012-11-28 23:28:08 +0800

    portable $PATH, HADOOP envvar

--- bin/zsh_basic.sh
+++ bin/zsh_basic.sh
@@ -8,11 +8,11 @@ alias d=popd
 
 export LESS="-FXRS"
 export EDITOR="ec"
-export PATH="/Users/joe/bin:/home/joe/bin:/usr/local/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:$PATH"
 
 if command -v brew ; then
    PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 fi
 
 export LC_ALL=en_US.UTF-8
-
+export HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce
