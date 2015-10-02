2e6ca801c4b6856513b0e810092f3e696459642a
crazyplum <crazyplum24@gmail.com>
2015-10-02 17:23:43 +0800

    modifiy sh_basic.sh

--- bin/sh_basic.sh
+++ bin/sh_basic.sh
@@ -1,6 +1,6 @@
 alias ls='ls --color'
 alias o='ls --color'
-alias M='mongo doraemon.iis.sinica.edu.tw/LJ40K'
+# alias M='mongo doraemon.iis.sinica.edu.tw/LJ40K'
 alias files='ls -1 . | wc -l'
 #alias oa='ls -a'
 #alias ol='ls -l'
@@ -11,30 +11,30 @@ alias files='ls -1 . | wc -l'
 
 alias l="ls -al --color"
 alias h="ls -ahl --color"
-alias p="PROJECT_HOME"
+#alias p="PROJECT_HOME"
 
 export LESS="-FXRS"
 export LC_ALL=en_US.UTF-8
 
-DEBEMAIL=$EMAIL
-DEBFULLNAME=$FULLNAME
-export DEBEMAIL DEBFULLNAME
-export GIT_AUTHOR_NAME="$FULLNAME"
-export GIT_COMMITTER_NAME="$FULLNAME"
+# DEBEMAIL=$EMAIL
+# DEBFULLNAME=$FULLNAME
+# export DEBEMAIL DEBFULLNAME
+# export GIT_AUTHOR_NAME="$FULLNAME"
+# export GIT_COMMITTER_NAME="$FULLNAME"
 
-if [ $(uname -s) = 'Darwin' ] && command -v brew >/dev/null ; then
-   PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
-fi
+# if [ $(uname -s) = 'Darwin' ] && command -v brew >/dev/null ; then
+#    PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# fi
 
-if [ $(uname -s) = 'Darwin' ] && command -v brew >/dev/null; then
-    if brew list | grep -x source-highlight > /dev/null ; then
-        export LESSOPEN="| $(brew --prefix source-highlight)/bin/src-hilite-lesspipe.sh %s"
-    fi
-else
-    if command -v src-hilite-lesspipe.sh; then
-        export LESSOPEN="| src-hilite-lesspipe.sh %s"
-    fi
-fi
+# if [ $(uname -s) = 'Darwin' ] && command -v brew >/dev/null; then
+#    if brew list | grep -x source-highlight > /dev/null ; then
+#        export LESSOPEN="| $(brew --prefix source-highlight)/bin/src-hilite-lesspipe.sh %s"
+#    fi
+# else
+#    if command -v src-hilite-lesspipe.sh; then
+#        export LESSOPEN="| src-hilite-lesspipe.sh %s"
+#    fi
+#fi
 
 
 # SCRIPT_DIR="$(dirname "$(readlink -f $0)")"
